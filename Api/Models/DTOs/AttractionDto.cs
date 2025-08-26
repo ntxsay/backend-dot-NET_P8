90fa906c69d7edcb7c1d414379c471f699672379
Loïc Bastaraud <39699697+ntxsay@users.noreply.github.com>
2025-08-26 07:47:57 -0400

    Ajout du DTO pour le controller

---
+++ Api/Models/DTOs/AttractionDto.cs
@@ -0,0 +1,11 @@
+﻿using GpsUtil.Location;
+
+namespace TourGuide.Models.DTOs;
+
+public class AttractionDto
+{
+    public required string AttractionName { get; set; }
+    public required Locations AttractionLocation { get; set; }
+    public required Locations UserLocation { get; set; }
+    public required double Distance { get; set; }
+}
